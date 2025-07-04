26f71e4b69b3393b11bc9335b150993713101738
NetAcad DEVASC <changeme@example.com>
2025-07-04 06:15:07 +0000

    Agregar script con nombres de los integrantes del grupo

---
+++ nombres_apellidos.py
@@ -0,0 +1,12 @@
+# nombres_integrantes.py
+
+# Lista de integrantes del grupo
+integrantes = [
+    "Lucas Del Solar",
+    "Eduardo Obreque"
+]
+
+# Imprimir la lista en pantalla
+print("Integrantes del grupo:")
+for nombre in integrantes:
+    print(f"- {nombre}")
